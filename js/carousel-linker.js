70891015da4222812cbffd0de18a940039695d7f
jonathanws <jon@jmdmobiledevstudios.com>
2017-12-07 01:31:45 -0500

    Added carousel Added scrolling jumbotron for later on main page

---
+++ js/carousel-linker.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("div.carousel-inner div.item").click(function() {
+        var link = $(this).attr('data');
+        if (link) {
+            window.location = link;
+        }
+          return false;
+    });
+});
